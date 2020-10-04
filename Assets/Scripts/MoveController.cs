2199bcaa5944beec12268e2bea85c3dbf07e68b6
ptagus <ptagusss@ya.ru>
2020-10-04 11:53:05 +0300

    addpackages

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveController : MonoBehaviour
+{
+    public NavMeshAgent nma;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                if (hit.collider.tag != "Finish")
+                {
+                    nma.destination = hit.point;
+                    Debug.Log(hit.collider.name);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UIBlock.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+using UnityEngine.EventSystems;
+using System.Security.Cryptography.X509Certificates;
+
+public class UIBlock : MonoBehaviour, IPointerDownHandler
+{
+    
+public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("work");
+    }
+}
