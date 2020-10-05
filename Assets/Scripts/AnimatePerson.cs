40d47d302a5a822c1ebeac5c500e7d3ffd899a55
ptagus <ptagusss@ya.ru>
2020-10-05 21:26:08 +0300

    model

---
+++ Assets/Scripts/AnimatePerson.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimatePerson : MonoBehaviour
+{
+    Animator animotor;
+    bool anitstop = false;
+    public UnityEngine.AI.NavMeshAgent nma;
+    public bool block = true;
+    RaycastHit hit;
+    Vector3 destin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animotor = GetComponent<Animator>();
+        nma.speed = 0.8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && block)
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                if (hit.collider.tag != "Finish")
+                {
+                    animotor.SetFloat("InputMagnitude", 0.25f);
+                    anitstop = true;
+                    nma.destination = hit.point;
+                    Debug.Log(hit.collider.name);
+                }
+            }
+        }
+        if (Mathf.Abs(Vector3.Distance(transform.position,nma.destination)) < 0.05f && anitstop)
+        {
+            anitstop = false;
+            animotor.SetFloat("InputMagnitude", 0f);
+        }
+    }
+
+    public void blockactive()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Window.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window : MonoBehaviour
+{
+    RoomMove rmove;
+    public GameObject[] SkyType;
+    public Sprite[] pictures;
+    public SpriteRenderer srpic;
+    public GameObject[] lights;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            SkyType[i].SetActive(false);
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            lights[0].SetActive(false);
+            lights[1].SetActive(false);
+        }
+        rmove = GetComponentInParent<RoomMove>();
+        if (rmove.nowTime < 6)
+        {
+            SkyType[0].SetActive(true);
+            srpic.sprite = pictures[Random.Range(1,20)];
+            lights[1].SetActive(true);
+        }
+        if (rmove.nowTime < 12 && rmove.nowTime > 5)
+        {
+            SkyType[1].SetActive(true);
+            srpic.sprite = pictures[Random.Range(1, 20)];
+            lights[0].SetActive(true);
+        }
+        if (rmove.nowTime < 18 && rmove.nowTime > 11)
+        {
+            SkyType[2].SetActive(true);
+            srpic.sprite = pictures[Random.Range(1, 20)];
+            lights[0].SetActive(true);
+        }
+        if (rmove.nowTime < 24 && rmove.nowTime > 18)
+        {
+            SkyType[3].SetActive(true);
+            srpic.sprite = pictures[Random.Range(1, 20)];
+            lights[0].SetActive(true);
+        }
+        if (rmove.nowTime == 18)
+        {
+            SkyType[3].SetActive(true);
+            srpic.sprite = pictures[0];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
